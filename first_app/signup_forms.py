a3c016b43d4dd04b4bd887cea45e50d57c29e16b
abrs7 <abrishsambong@gmail.com>
2024-02-15 12:16:40 +0300

    Signup page updated

---
+++ first_app/signup_forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.core import validators
+# from first_app import User
+from first_app.models import User
+
+class NewUser(forms.ModelForm):
+    class Meta():
+        model = User
+        fields = '__all__'
